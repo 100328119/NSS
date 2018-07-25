fa97c17ff1a70294edef854b2fb5679e5720949b
kun Huang <KUHUANG@bcliquor.com>
2018-07-25 16:40:17 -0700

    7/25

--- public/js/network.js
+++ public/js/network.js
@@ -9,7 +9,6 @@ class Network extends React.Component{
 
     this.getNetworkData = this.getNetworkData.bind(this);
     this.OnEdit = this.OnEdit.bind(this);
-
   }
   getNetworkData(){
     fetch('/Netdata/bcls/1',{
@@ -42,7 +41,7 @@ class Network extends React.Component{
       <div>
         <h1>{this.state.network.Category} - {this.state.network.ID}</h1>
         <hr/>
-        <EndDeviceTable EndDevices={this.state.network.End_Device} EditState={this.state.EditState} OnEdit={this.OnEdit}/>
+        <EndDeviceTable End_ID={this.state.network.ID} EditState={this.state.EditState} OnEdit={this.OnEdit}/>
       </div>
     );
   }
@@ -51,77 +50,85 @@ class Network extends React.Component{
 class EndDeviceTable extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      EndDevices:[],
+      EditState: false,
+      CurrentEdit:-1
+    }
+    this.getEndDevice = this.getEndDevice.bind(this);
+    this.OnEdit = this.OnEdit.bind(this);
   }
-  render(){
-    // const _EndDevices = JSON.parse(this.props.EndDevices);
-    const end =this.props.EndDevices;
-    const Sample = [
-      {
-        'IP':'10.44.2.2',
-        'D_Name':'LDR01002',
-        'VLAN':20,
-        'Port':'F0/3',
-        'Active':true,
-        'Description':"Register 1",
-        'Type':'POS Register',
-        'Make':'Zabar',
-        'Model':'Register',
-        'Connected_Device':'LDBRR002'
-      },
-      {
-        'IP':'10.44.2.17',
-        'D_Name':'LDSDC002',
-        'VLAN':20,
-        'Port':'F0/13',
-        'Active':true,
-        'Description':"BCLDB RO Domain Controller",
-        'Type':'Server',
-        'Make':'Zabar',
-        'Model':'server',
-        'Connected Device':'LDBRR002'
-      },
-      {
-        'IP':'10.44.2.13',
-        'D_Name':'LDSDC002',
-        'VLAN':20,
-        'Port':'F0/13',
-        'Active':true,
-        'Description':"BCLDB RO Domain Controller",
-        'Type':'Server',
-        'Make':'Zabar',
-        'Model':'server',
-        'Connected Device':'LDBRR002'
-      }
-    ];
-    const EditState = this.props.EditState;
-    // console.log(_EndDevices);
-    // console.log(end);
-    console.log(Sample);
-    const End_DeviceRow = Sample.map(function(ed){
-      return (
-        <li>{ed.IP}</li>
-      );
+
+  getEndDevice(){
+    fetch('/EndDevice/'+this.props.End_ID,{
+      method:'GET'
+    }).then(response=>{
+      return response.json();
+    }).then(data=>{
+      this.setState({
+        EndDevices:data
+      });
     });
+  }
+
+  OnEdit(){
+    if(this.state.EditState === false){
+      this.setState({
+          EditState:true
+      })
+    }else{
+      this.setState({
+          EditState:false
+      })
+    }
+  }
+
+  componentDidMount(){
+    this.getEndDevice();
+  }
+
+  render(){
+    // console.log(Sample);
+    const End_DeviceRow = this.state.EndDevices.map((endDeice, index) =>
+      <tr key={index}>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.IP}/>):(endDeice.IP)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.D_Name}/>):(endDeice.D_Name)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.VLAN}/>):(endDeice.VLAN)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Port}/>):(endDeice.Port)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Active}/>):(endDeice.Active)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Description}/>):(endDeice.Description)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Type}/>):(endDeice.Type)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Make}/>):(endDeice.Make)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Model}/>):(endDeice.Model)}</td>
+          <td>{this.state.EditState ? (<input defaultValue={endDeice.Connected_Device}/>):(endDeice.Connected_Device)}</td>
+          <td>
+          <button onClick={this.OnEdit}>{this.state.EditState ? "Save" : "Edit" }</button>
+          <button> Delete </button>
+          </td>
+      </tr>
+    );
     return(
     <div>
-    <table>
-      <thead>
-        <tr>
-          <th>IP</th>
-          <th>Device Name</th>
-          <th>VLAN</th>
-          <th>Port</th>
-          <th>Description</th>
-          <th>Type</th>
-          <th>Make</th>
-          <th>Model</th>
-          <th>Connected Device</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-        <tbody>
-        </tbody>
-      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>IP</th>
+            <th>Device Name</th>
+            <th>VLAN</th>
+            <th>Port</th>
+            <th>Active</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Connected Device</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+          <tbody>
+            {End_DeviceRow}
+          </tbody>
+        </table>
     </div>
   );
   }
