dc2e05d779fc1ed8e13a759cc52a24c8f3e07402
kun Huang <KUHUANG@bcliquor.com>
2018-07-26 18:23:49 -0700

    fk react

---
+++ public/js/networkpage/network.js
@@ -0,0 +1,155 @@
+class Network extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      network:[],
+      EditState:false,
+      currentEdit:-1
+    };
+
+    this.getNetworkData = this.getNetworkData.bind(this);
+    this.OnEdit = this.OnEdit.bind(this);
+  }
+  getNetworkData(){
+    fetch('/Netdata/bcls/1',{
+      method: 'GET',
+    }).then(response => {
+      return response.json();
+    }).then(data => {
+      this.setState({
+        network: data
+      });
+    });
+  }
+  OnEdit(){
+    if(this.state.EditState === false){
+      this.setState({
+          EditState:true
+      })
+    }else{
+      this.setState({
+          EditState:false
+      })
+    }
+  }
+  componentDidMount() {
+    this.getNetworkData();
+  }
+  render(){
+    console.log(this.state.network.End_Device);
+    return(
+      <div>
+        <h1>{this.state.network.Category} - {this.state.network.ID}</h1>
+        <hr/>
+        <EndDeviceTable />
+      </div>
+    );
+  }
+}
+
+class EndDeviceTable extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      EndDevices:[],
+      Err:{'err':'err'},
+      EditState: false,
+      CurrentEdit:-1
+    };
+    this.getEndDevice = this.getEndDevice.bind(this);
+    this.OnEdit = this.OnEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  getEndDevice(){
+    fetch('/EndDevice/'+this.props.End_ID,{
+      method:'GET'
+    }).then(response=>{
+      return response.json();
+    }).then(data=>{
+      this.setState({
+        EndDevices:data
+      });
+    });
+  }
+
+  OnEdit(index){
+    if(this.state.EditState === false){
+      this.setState({
+          EditState:true,
+          CurrentEdit:index
+      })
+    }else{
+      this.setState({
+          EditState:false,
+          CurrentEdit:-1
+      });
+    }
+  }
+
+  handleChange(e,name,i){
+    const { value } = e.target;
+    this.setState(state => ({
+      EndDevices: state.EndDevices.map(
+        (row, j) => (j === i ? {...row, [name]:value }:row)
+      )
+    }));
+  }
+
+  componentDidMount(){
+    this.getEndDevice();
+  }
+
+  render(){
+    // console.log(Sample);
+    const End_DeviceRow = this.state.EndDevices.map((endDeice, index) =>
+      <tr key={index}>
+          {Object.keys(endDeice).map((key) => {
+              return (
+                <td key={key}>{this.state.CurrentEdit === index  ? (<input className="form-control" name={key} onChange={(e)=> this.handleChange(e,key,index)} defaultValue={endDeice[key]}/>):(endDeice[key])}</td>
+              )
+          })}
+          <td>
+          <button onClick={()=>this.OnEdit(index)}>{this.state.CurrentEdit === index ? "Save" : "Edit" }</button>
+          <button> Delete </button>
+          </td>
+      </tr>
+    );
+    // const Column = (this.state.EndDevices[0] === null) ? this.state.Err: this.state.EndDevices[0];
+    // console.log(Column);
+    // const table_Column = Object.keys(Column).map((key,index)=>{
+    //   return(
+    //     <th key={index}>{key}</th>
+    //   )
+    // });
+    return(
+    <div className="card">
+      <div className='card-header'>testing</div>
+      <div className='card-body'>
+      <table className="table table-striped table-bordered table-hover" >
+        <thead>
+          <tr>
+          <th>IP</th>
+          <th>Device Name</th>
+          <th>VLAN</th>
+          <th>Port</th>
+          <th>Active</th>
+          <th>Description</th>
+          <th>Type</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Connected Device</th>
+          <th>Action</th>
+          </tr>
+        </thead>
+          <tbody>
+            {End_DeviceRow}
+          </tbody>
+        </table>
+        </div>
+    </div>
+  );
+  }
+}
+
+ReactDOM.render(<Network />, document.getElementById('network'))
