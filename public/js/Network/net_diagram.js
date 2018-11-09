16e27fb4ea49af66ca039c59778b271648d27110
Kun Huang <KUHUANG@bcliquor.com>
2018-11-09 13:56:49 -0800

    network diagram bug fix

--- public/js/Network/net_diagram.js
+++ public/js/Network/net_diagram.js
@@ -10,13 +10,11 @@ nss.controller("diagram", function($scope, $http,$location,$window,$filter){
         $http.get('/api/Netdata/Site/'+$scope.id)
           .then(function(response){
             $scope.Network = angular.copy(response.data);
-            console.log(response.data);
-              $scope.loadDiagram();
+            $scope.loadDiagram();
         });
         //get vlan Information
         $http.get('/api/Netdata/vlan')
           .then(function successCallback(res){
-            console.log(res.data);
             $scope.vlans = angular.copy(res.data);
           }, function errorCallback(res){
             console.log(res.data);
@@ -30,8 +28,17 @@ nss.controller("diagram", function($scope, $http,$location,$window,$filter){
      $scope.nodes = [];
      // Create a data table with links.
      $scope.edges = [];
+
+     var id_array = [];
      for(var i = 0; i < $scope.Network.WANs.length;  i++ ){
-       $scope.nodes.push({id:$scope.Network.WANs[i].Provider, label:$scope.Network.WANs[i].Provider+' '+$scope.Network.WANs[i].WAN_IP, image: DIR + 'wan.png', shape: 'image'});
+       if(!id_array.includes($scope.Network.WANs[i].Provider)){
+          $scope.nodes.push({id:$scope.Network.WANs[i].Provider, label:$scope.Network.WANs[i].Provider+' '+$scope.Network.WANs[i].WAN_IP, image: DIR + 'wan.png', shape: 'image'});
+          id_array.push($scope.Network.WANs[i].Provider);
+       }else{
+          $scope.nodes.push({id:$scope.Network.WANs[i].Provider+i, label:$scope.Network.WANs[i].Provider+' '+$scope.Network.WANs[i].WAN_IP, image: DIR + 'wan.png', shape: 'image'});
+          id_array.push($scope.Network.WANs[i].Provider+i);
+       }
+
      }
      for(var i = 0; i < $scope.Network.Net_devices.length;  i++ ){
        if($scope.Network.Net_devices[i].type == 'Router'){
@@ -43,19 +50,18 @@ nss.controller("diagram", function($scope, $http,$location,$window,$filter){
          }else{
            $scope.edges.push({from: $scope.Network.Net_devices[i].name, to: $scope.Network.Net_devices[i].Connect_Device, length: EDGE_LENGTH_MAIN});
          }
+
        }else{
          $scope.nodes.push({id:$scope.Network.Net_devices[i].name, label:$scope.Network.Net_devices[i].name+' '+$scope.Network.Net_devices[i].Loopback, image: DIR + 'switch.png', shape: 'image'});
          $scope.edges.push({from: $scope.Network.Net_devices[i].name, to: $scope.Network.Net_devices[i].Connect_Device, length: EDGE_LENGTH_MAIN});
          }
        }
-     console.log($scope.nodes);
      // create a network
      $scope.container = document.getElementById('mynetwork');
      $scope.data = {
        nodes: $scope.nodes,
        edges: $scope.edges
      };
-     console.log($scope.data);
      $scope.options = {};
      $scope.network = new vis.Network($scope.container, $scope.data, $scope.options);
     }
